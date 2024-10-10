06e1d36fe633d0a003ae966e9ef58afa9a8194a8
imperfectsworld <imperfectsworld@gmail.com>
2024-10-10 15:21:46 -0400

    Created Controllers and Classes for User and Item

---
+++ Karma Backend/Models/Community.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Karma_Backend.Models;
+
+public partial class Community
+{
+    public int CommunityId { get; set; }
+
+    public int? ZipCode { get; set; }
+    [JsonIgnore]
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
