b59c424d91b736f5c67546fe554ce7b617df5afd
kingDVD <davidvgoodwin@gmail.com>
2024-10-16 14:29:15 -0400

    deleted post method involving BLOB file output.

--- Karma Backend/Controllers/ItemController.cs
+++ Karma Backend/Controllers/ItemController.cs
@@ -22,7 +22,7 @@ namespace Karma_Backend.Controllers
         [HttpPost()]
 
         public IActionResult AddItem([FromBody] Item newItem)
-        { 
+        {
             newItem.Id = 0;
             dbContext.Items.Add(newItem);
 
@@ -31,25 +31,7 @@ namespace Karma_Backend.Controllers
 
         }
 
-        //[HttpPost("ItemWithImage")]
-        //public async Task<IActionResult> AddItemWithImage([FromForm] ItemCreateDTO itemDto)
-        //{
-        //    if (itemDto.file != null && itemDto.file.Length > 0)
-        //    {
-        //        var fileName = Path.GetFileName(itemDto.file.FileName);
-        //        var filePath = Path.Combine("YourFileStoragePath", fileName);
 
-        //        using (var stream = new FileStream(filePath, FileMode.Create))
-        //        {
-        //            await itemDto.file.CopyToAsync(stream);
-        //        }
-        //        itemDto.item.Pic = filePath;
-        //    }
-        //    _context.Items.Add(itemDto.item);
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok(itemDto.item);
-        //}
     }
 
 }
